16652f8543335f706103269b62d45da2b1c7feb5
Zaki Wu <134594806+Zaki-Wu-2004@users.noreply.github.com>
2025-01-24 01:01:37 +0800

    Update and rename train40 (2).sh to the_well/benchmark/train.sh

--- the_well/benchmark/train.sh
+++ the_well/benchmark/train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=asolver_well_h100_nrmse
+#SBATCH --job-name=fno_h100
 #SBATCH --mail-user=z.wu@stu.pku.edu.cn
 #SBATCH --mail-type=ALL
 #SBATCH --cpus-per-task=1
@@ -10,7 +10,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=128gb
 #SBATCH --time=23:59:59
-#SBATCH --output=/home/zw474/project/LOLL/the_well/the_well/ah100job/active_matter_nrmse_asolver2_%J.log
+#SBATCH --output=/home/zw474/project/LOLL/the_well/the_well/h100job/active_matter_fno_%J.log
 
 # export OMP_NUM_THREADS=${SLURM_CPUS_ON_NODE}
 
@@ -25,4 +25,4 @@ cd /home/zw474/project/LOLL/the_well/the_well/benchmark
 
 # source /mnt/home/polymathic/ceph/the_well/venv_benchmark_well/bin/activate
 # Launch the training script
-HYDRA_FULL_ERROR=1 python train.py experiment=asolver_v2 server=local data=active_matter
+HYDRA_FULL_ERROR=1 python train.py experiment=fno server=local data=active_matter
